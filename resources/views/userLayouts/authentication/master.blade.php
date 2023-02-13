c08b110b719613ba573a84dfeb7923b0c4be9701
IDCDEV03 <idcdev03@iddrives.co.th>
2023-02-13 15:02:20 +0700

    update

--- resources/views/userLayouts/authentication/master.blade.php
+++ resources/views/userLayouts/authentication/master.blade.php
@@ -4,12 +4,12 @@
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="SP INTER TOUR จองแพ็คเกจทัวร์ จัดสมมนา ศึกษาดูงาน ปรึกษาวีซ่า ตั๋วเครื่องบิน">
-    <meta name="keywords" content="จองแพ็คเกจทัวร์ จัดสมมนา ศึกษาดูงาน ปรึกษาวีซ่า ตั๋วเครื่องบิน แปลเอกสาร">
-    <meta name="author" content="SP Inter Tour">
+    <meta name="description" content="ธัญวรัตม์ ทราเวล จองแพ็คเกจทัวร์ บริการรถบัสให้เช่า">
+    <meta name="keywords" content="จองแพ็คเกจทัวร์ บริการรถบัสให้เช่า">
+    <meta name="author" content="Thanwarat Travel">
     <link rel="icon" href="{{asset('assets/images/favicon.png')}}" type="image/x-icon">
     <link rel="shortcut icon" href="{{asset('assets/images/favicon.png')}}" type="image/x-icon">
-     <title>SP Inter Tour - @yield('title')</title>
+     <title>Thanwarat Travel - @yield('title')</title>
     <!-- Google font-->
     <link href="https://fonts.googleapis.com/css?family=Rubik:400,400i,500,500i,700,700i&amp;display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Roboto:300,300i,400,400i,500,500i,700,700i,900&amp;display=swap" rel="stylesheet">
